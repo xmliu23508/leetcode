33851ee3073366358e211b11c78ec0d9b34c63de
lxming <xiaoming.liu@gmail.com>
2015-07-27 14:01:12 -0400

    improve format

--- addparentheses/Solution.java
+++ addparentheses/Solution.java
@@ -11,30 +11,30 @@ public class Solution {
 		return this.helper(input, cache);
 	}
 
-	List<Integer> helper(String s, HashMap<String, List<Integer>> cache) {
-		if (cache.get(s) != null) {
-			return cache.get(s);
-		}
-		boolean isExpression = false;
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		for (int i = 0; i < s.length(); i++) {
-			if ("+-*".indexOf(s.charAt(i)) != -1) {
-				List<Integer> left = helper(s.substring(0, i), cache);
-				List<Integer> right = helper(s.substring(i + 1), cache);
-				for (Integer l : left) {
-					for (Integer r : right) {
-						result.add(cal(l, r, s.charAt(i)));
-					}
-				}
-				isExpression = true;
-			}
-		}
-		if (!isExpression) {
-			result.add(Integer.parseInt(s));
-		}
-		cache.put(s, result);
-		return result;
-	}
+    List<Integer> helper(String s, HashMap<String, List<Integer>> cache) {
+        if (cache.get(s) != null) {
+            return cache.get(s);
+        }
+        boolean isExpression = false;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if ("+-*".indexOf(s.charAt(i)) != -1) {
+                List<Integer> left = helper(s.substring(0, i), cache);
+                List<Integer> right = helper(s.substring(i + 1), cache);
+                for (Integer l : left) {
+                    for (Integer r : right) {
+                        result.add(cal(l, r, s.charAt(i)));
+                    }
+                }
+                isExpression = true;
+            }
+        }
+        if (!isExpression) {
+            result.add(Integer.parseInt(s));
+        }
+        cache.put(s, result);
+        return result;
+    }
 
 	int cal(int l, int r, char op) {
 		int result = 0;
