69be0626b56ba88cccd319dc498118b8882edb68
lxming <xiaoming.liu@gmail.com>
2015-08-01 15:14:17 -0400

    same_tree

---
+++ same_tree/Solution.java
@@ -0,0 +1,11 @@
+package same_tree;
+
+import common.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+         if(p == null || q == null) return false;
+         return p.val==q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+     }
+}
